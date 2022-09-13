7c321d7542fee337d864a85467601aa1053c0ca7
Gz <354300537@qq.com>
2022-09-13 22:55:03 +0800

    嵌套effect探索

--- __tests__/effect.ts
+++ __tests__/effect.ts
@@ -165,5 +165,54 @@ describe('effect', () => {
 
   })
 
+  it.skip('嵌套的effect', () => {
+    const people = reactive({ age: 20, name: 'test' })
+
+    const nameFn = jest.fn(function nameFnJest() {
+      const name = people.name + ' _name_ '
+    })
+    const ageFn = jest.fn(function ageFnJest() {
+      const age = people.age + 1
+      effect(nameFn)
+    })
+
+    effect(ageFn)
+    expect(ageFn).toBeCalledTimes(1)
+    expect(nameFn).toBeCalledTimes(1)
+    
+    people.age = 30
+    expect(ageFn).toBeCalledTimes(2)
+    expect(nameFn).toBeCalledTimes(2)
+    
+    people.name = 'new Name'
+    expect(ageFn).toBeCalledTimes(2)
+    expect(nameFn).toBeCalledTimes(3)
+
+
+  })
+
+  it.skip('嵌套effect', () => {
+     // 原始数据
+     const data = { foo: true, bar: true }
+     // 代理对象
+     const obj = reactive(data)
+    
+     // 全局变量
+     let temp1, temp2
+    
+     // effectFn1 嵌套了 effectFn2
+     effect(function effectFn1() {
+       console.log('effectFn1 执行')
+    
+       effect(function effectFn2() {
+         console.log('effectFn2 执行')
+         // 在 effectFn2 中读取 obj.bar 属性
+         temp2 = obj.bar
+       })
+       // 在 effectFn1 中读取 obj.foo 属性
+       temp1 = obj.foo
+     })
+     obj.foo = false
+  })
 
 })
